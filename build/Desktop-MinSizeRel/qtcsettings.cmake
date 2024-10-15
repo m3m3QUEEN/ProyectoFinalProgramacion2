d67513555699bbea47f88876e668dc0beb1a2313
m3m3QUEEN <a.zapata4@utp.edu.co>
2024-10-15 17:19:39 -0500

    hola

---
+++ build/Desktop-MinSizeRel/qtcsettings.cmake
@@ -0,0 +1,9 @@
+# This file is managed by Qt Creator, do not edit!
+
+set("CMAKE_CXX_COMPILER" "C:/msys64/ucrt64/bin/g++.exe" CACHE "FILEPATH" "" FORCE)
+set("QT_QMAKE_EXECUTABLE" "" CACHE "FILEPATH" "" FORCE)
+set("CMAKE_PREFIX_PATH" "" CACHE "PATH" "" FORCE)
+set("CMAKE_BUILD_TYPE" "MinSizeRel" CACHE "STRING" "" FORCE)
+set("CMAKE_PROJECT_INCLUDE_BEFORE" "C:/Users/jesui/OneDrive/Documentos/MusicPlayer/build/Desktop-MinSizeRel/.qtc/package-manager/auto-setup.cmake" CACHE "FILEPATH" "" FORCE)
+set("CMAKE_C_COMPILER" "C:/msys64/ucrt64/bin/gcc.exe" CACHE "FILEPATH" "" FORCE)
+set("CMAKE_GENERATOR" "Ninja" CACHE "STRING" "" FORCE)
